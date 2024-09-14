3a3aa7369da2aaf677d1d8c618d0db7465e8dcd9
pmpr.dev <pmpr.dev@gmail.com>
2024-09-14 19:46:05 +0000

    Obfuscated

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             66e5e7dad4357             |
+    |_______________________________________|
+*/
+ use Pmpr\Module\WoocommerceGateway\WoocommerceGateway; WoocommerceGateway::symcgieuakksimmu();
---
+++ src/Pmpr/Module/WoocommerceGateway/Data/ClientSalePaymentRequest.php
@@ -0,0 +1,7 @@
+<?php
+/*   _______________________________________
+    |  Obfuscated by PMPR - Php Obfuscator  |
+    |             66e5e7dad4357             |
+    |_______________________________________|
+*/
+ namespace Pmpr\Module\WoocommerceGateway\Data; class ClientSalePaymentRequest { public $Amount; public $OrderId; public $CallBackUrl; public $LoginAccount; public $AdditionalData; public function qiccuiwooiquycsg(array $yiosiwewiecqmkaa) { foreach ($yiosiwewiecqmkaa as $csgiecsagosuucqo => $eqgoocgaqwqcimie) { if (!isset($this->{$csgiecsagosuucqo})) { goto wusciwkkckmqigms; } $this->{$csgiecsagosuucqo} = $eqgoocgaqwqcimie; wusciwkkckmqigms: kqswcsysqawkcgye: } ewscugeuicukkycc: } }

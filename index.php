9b9ab385848e6e7eaee5dae22ed4197fb3d38b88
pmpr.dev <pmpr.dev@gmail.com>
2024-12-28 20:22:22 +0000

    Obfuscated

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*   _______________________________________
     |  Obfuscated by PMPR - Php Obfuscator  |
-    |             670d4b8e91e7f             |
+    |             67705dd617fd6             |
     |_______________________________________|
 */
  use Pmpr\Module\WoocommerceGateway\WoocommerceGateway; WoocommerceGateway::symcgieuakksimmu();
